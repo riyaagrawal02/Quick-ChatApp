6b9ffc2e98324cd042630068ef1abb09197a7b1b
riyaagrawal-0025 <riyaagrawal0205@gmail.com>
2026-03-20 02:40:03 +0530

    update login page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -31,7 +31,6 @@ const ProfilePage = () => {
    
   }
 
-
   return (
     <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center' >
       <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg' >
