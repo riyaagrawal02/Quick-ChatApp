086370c343ebff8d776a95e6d54adda6ce69986d
riyaagrawal-0025 <riyaagrawal0205@gmail.com>
2026-03-20 02:59:22 +0530

    bgimage fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import React, { useContext } from 'react'
-import {Navigate, Route, Routes} from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import ProfilePage from './pages/ProfilePage'
 import LoginPage from './pages/LoginPage'
-import {Toaster} from "react-hot-toast"
+import { Toaster } from "react-hot-toast"
 import { AuthContext } from '../context/AuthContext'
+import assets from './assets/assets'
 
 const App = () => {
-  const {authUser} = useContext(AuthContext)
+  const { authUser } = useContext(AuthContext)
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')]  bg-contain">
-      <Toaster/>
+    <div
+      className="bg-contain"
+      style={{ backgroundImage: `url(${assets.bgImage})` }}
+    >
+      <Toaster />
       <Routes>
-        <Route path='/' element={authUser ?<HomePage/> : <Navigate to="/login" />} />
-        <Route path='/login' element={!authUser ? <LoginPage/> : <Navigate to="/" />} />
-        <Route path='/profile' element={authUser ? <ProfilePage/> : <Navigate to="/login" /> } />
+        <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
 
       </Routes>
     </div>
